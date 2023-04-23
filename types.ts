5e7b45c633ff3dda1d0e6b766a4b2ff0dedec77f
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-04-23 13:59:15 +0900

    docs(types): add code description

--- types.ts
+++ types.ts
@@ -1,11 +1,21 @@
 // Copyright 2023-latest the httpland authors. All rights reserved. MIT license.
 // This module is browser compatible.
 
+/** Authorization API. */
 export interface Authorization {
+  /** Authentication scheme. */
   readonly authScheme: string;
-  readonly token: string | Record<string, string> | null;
+
+  /** token68 or auth-param. */
+  readonly token: Token68 | AuthParam | null;
 }
 
+/** Representation of [token68](https://www.rfc-editor.org/rfc/rfc9110.html#auth.params). */
+export type Token68 = string;
+
+/** Pair of name and value.
+ * Representation of [auth-param](https://www.rfc-editor.org/rfc/rfc9110.html#section-11.2-5).
+ */
 export interface AuthParam {
   readonly [k: string]: string;
 }
